1c0531a47b504d780e3f2ca04faa5ca23576a9a1
varsh <varsh@VARSHITA>
2024-06-23 20:59:28 +0530

    Add project files.

---
+++ RobotShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RobotShop.Models;
+
+namespace RobotShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        } 
+        public DbSet<Brand> Brand { get; set; }
+    }
+}
